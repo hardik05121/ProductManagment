594a763f97ddb417d1ca24245f492e87f992d36a
Hardik <hardikkanzariya05121@gmail.com>
2023-07-01 16:18:51 +0530

    Add Validation On some model.

--- ProductManagment_Models/Models/Supplier.cs
+++ ProductManagment_Models/Models/Supplier.cs
@@ -11,16 +11,25 @@ public partial class Supplier
     [Key]
     public int Id { get; set; }
 
-    [StringLength(50)]
+    [StringLength(50, MinimumLength = 3)]
+    [Display(Name = "SupplierName*")]
     public string SupplierName { get; set; } = null!;
 
     [StringLength(50)]
+    [DataType(DataType.PhoneNumber)]
+    [Display(Name = "PhoneNumber*")]
+    [RegularExpression(@"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$", ErrorMessage = "Invalid Phone number")]
     public string? ContactPerson { get; set; }
 
     [StringLength(450)]
+    [DataType(DataType.EmailAddress)]
+    [RegularExpression(@"[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}", ErrorMessage = "Invalid Email Address")]
     public string? Email { get; set; }
 
     [StringLength(450)]
+    [Required(ErrorMessage = "Please enter the product URL.")]
+    [RegularExpression(@"^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$",
+        ErrorMessage = "Please enter a valid URL.")]
     public string? WebSite { get; set; }
 
     public long MobileNumber { get; set; }
@@ -28,15 +37,20 @@ public partial class Supplier
     public long? PhoneNumber { get; set; }
 
     [StringLength(450)]
+    [Display(Name = "Your Address*")]
     public string? Address { get; set; }
 
+    [Display(Name = "Select Country*")]
     public int CountryId { get; set; }
 
+    [Display(Name = "Select State*")]
     public int StateId { get; set; }
 
+    [Display(Name = "Select City*")]
     public int CityId { get; set; }
 
     [StringLength(450)]
+    [Display(Name = "Enter Billing Address*")]
     public string BillingAddress { get; set; } = null!;
 
     public int BillingCountryId { get; set; }
@@ -46,6 +60,7 @@ public partial class Supplier
     public int BillingCityId { get; set; }
 
     [StringLength(450)]
+    [Display(Name = "Enter Shipping Address*")]
     public string ShippingAddress { get; set; } = null!;
 
     public int ShippingCountryId { get; set; }
@@ -96,3 +111,38 @@ public partial class Supplier
     [InverseProperty("SupplierStates")]
     public virtual State State { get; set; } = null!;
 }
+
+
+
+//worked string;-
+
+//[Display(Name = "Price for 50+")]
+
+//[Range(1, 1000, ErrorMessage = "Please enter a value between 1 and 1000")]
+
+//[StringLength(60, MinimumLength = 3)]
+
+//[DataType(DataType.Date)]
+//[DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+//[DisplayFormat(DataFormatString = "{0:d}", ApplyFormatInEditMode = true)]
+
+//[DataType(DataType.PhoneNumber)]
+//[RegularExpression(@"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$", ErrorMessage = "Invalid Phone number")]
+//[StringLength(13, MinimumLength = 10)]
+//[RegularExpression(@"^(0|91)?[6-9][0-9]{9}$", ErrorMessage = "Invalid Mobile Number.")]
+//[RegularExpression(@"^([\+]?33[-]?|[0])?[1-9][0-9]{8}$", ErrorMessage = "Invalid Mobile Number.")]
+
+//[StringLength(50)]
+//[DataType(DataType.EmailAddress)]
+//[RegularExpression(@"[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}", ErrorMessage = "Invalid Email Address")]
+//public string? Email { get; set; }
+
+//[Required(ErrorMessage = "Please enter the product URL.")]
+//[RegularExpression(@"^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$",
+//    ErrorMessage = "Please enter a valid URL.")]
+//public string? WebSite { get; set; }
+
+//[Display(Name = "Joining Date")]
+//[DataType(DataType.Date)]
+//[DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}",ApplyFormatInEditMode = true)]
+//public DateTime JoiningDate { get; set; }
--- ProductManagment_Models/Models/Warehouse.cs
+++ ProductManagment_Models/Models/Warehouse.cs
@@ -12,16 +12,23 @@ public partial class Warehouse
     public int Id { get; set; }
 
     [StringLength(50)]
+    [Display(Name = "WarehouseName*")]
     public string WarehouseName { get; set; } = null!;
 
+    [DataType(DataType.PhoneNumber)]
+    [Display(Name = "ContactPerson*")]
+    [RegularExpression(@"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$", ErrorMessage = "Invalid Phone Number And Enter a number For 10 digit.")]
     public long? ContactPerson { get; set; }
+
 
     public long? MobileNumber { get; set; }
 
     [StringLength(50)]
+    [DataType(DataType.EmailAddress)]
+
+    [RegularExpression(@"[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}", ErrorMessage = "Invalid Email Address")]
     public string? Email { get; set; }
 
-    [StringLength(50)]
     public string? Address { get; set; }
 
     public bool IsActive { get; set; }
@@ -31,4 +38,28 @@ public partial class Warehouse
 
     [InverseProperty("Warehouse")]
     public virtual ICollection<Product> Products { get; set; } = new List<Product>();
+
 }
+
+// worked string;-
+
+//[Display(Name = "Price for 50+")]
+
+//[Range(1, 1000, ErrorMessage = "Please enter a value between 1 and 1000")]
+
+//[StringLength(60, MinimumLength = 3)]
+
+//[DataType(DataType.Date)]
+//[DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}",ApplyFormatInEditMode = true)]
+//[DisplayFormat(DataFormatString = "{0:d}", ApplyFormatInEditMode = true)]
+
+//[DataType(DataType.PhoneNumber)]
+//[RegularExpression(@"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$", ErrorMessage = "Invalid Phone number")]
+//[StringLength(13, MinimumLength = 10)]
+//[RegularExpression(@"^(0|91)?[6-9][0-9]{9}$", ErrorMessage = "Invalid Mobile Number.")]
+//[RegularExpression(@"^([\+]?33[-]?|[0])?[1-9][0-9]{8}$", ErrorMessage = "Invalid Mobile Number.")]
+
+//[StringLength(50)]
+//[DataType(DataType.EmailAddress)]
+//[RegularExpression(@"[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}", ErrorMessage = "Invalid Email Address")]
+//public string? Email { get; set; }
