657a6c63ce45a56bed0381960f1a76d6c43f7aeb
Henil Patel <patelhenilk2001@gmail.com>
2023-07-10 15:49:39 +0530

    Add Search and Pagiination

---
+++ Bachup ProductManagment/ProductManagmentWeb/Areas/Admin/Controllers/ExpenseCategoryController.cs
@@ -0,0 +1,126 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ProductManagment_DataAccess.Repository.IRepository;
+using ProductManagment_Models.Models;
+using System.Data;
+using System.Drawing.Drawing2D;
+
+namespace ProductManagmentWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize]
+    public class ExpenseCategoryController : Controller
+    {
+
+        private readonly IUnitOfWork _unitOfWork;
+        public ExpenseCategoryController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        #region Index
+        public IActionResult Index()
+        {
+            List<ExpenseCategory> objExpenseCategoryList = _unitOfWork.ExpenseCategory.GetAll().ToList();
+
+            return View(objExpenseCategoryList);
+        }
+
+        #endregion
+
+        #region Upsert
+        public IActionResult Upsert(int? id)
+        {
+
+            if (id == null || id == 0)
+            {
+                //create
+                return View(new ExpenseCategory());
+            }
+            else
+            {
+                //update
+                ExpenseCategory expenseCategory = _unitOfWork.ExpenseCategory.Get(u => u.Id == id);
+                return View(expenseCategory);
+            }
+
+        }
+
+        [HttpPost]
+        public IActionResult Upsert(ExpenseCategory expenseCategory)
+        {
+            if (ModelState.IsValid)
+            {
+
+                if (expenseCategory.Id == 0)
+                {
+
+                    ExpenseCategory expenseCategoryobj = _unitOfWork.ExpenseCategory.Get(u => u.ExpenseCategoryName == expenseCategory.ExpenseCategoryName);
+                    if (expenseCategoryobj != null)
+                    {
+                        TempData["error"] = "ExpenseCategory Name Already Exist!";
+                    }
+                    else
+                    {
+                        _unitOfWork.ExpenseCategory.Add(expenseCategory);
+                        _unitOfWork.Save();
+                        TempData["success"] = "ExpenseCategory created successfully";
+                    }
+
+                }
+                else
+                {
+                    ExpenseCategory expenseCategoryobj = _unitOfWork.ExpenseCategory.Get(u => u.Id != expenseCategory.Id && u.ExpenseCategoryName == expenseCategory.ExpenseCategoryName);
+                    if (expenseCategoryobj != null)
+                    {
+                        TempData["error"] = "ExpenseCategory Already Exist!";
+                    }
+                    else
+                    {
+                        _unitOfWork.ExpenseCategory.Update(expenseCategory);
+                        _unitOfWork.Save();
+                        TempData["success"] = "ExpenseCategory Updated successfully";
+                    }
+
+                }
+
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(expenseCategory); 
+            }
+
+         
+        }
+
+        #endregion
+
+        #region API CALLS
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<ExpenseCategory> objExpenseCategoryList = _unitOfWork.ExpenseCategory.GetAll().ToList();
+            return Json(new { data = objExpenseCategoryList });
+        }
+
+
+        [HttpDelete]
+        public IActionResult Delete(int? id)
+        {
+            var ExpenseCategoryToBeDeleted = _unitOfWork.ExpenseCategory.Get(u => u.Id == id);
+            if (ExpenseCategoryToBeDeleted == null)
+            {
+                return Json(new { success = false, message = "Error while deleting" });
+            }
+
+            _unitOfWork.ExpenseCategory.Remove(ExpenseCategoryToBeDeleted);
+            _unitOfWork.Save();
+
+            return Json(new { success = true, message = "Delete Successful" });
+        }
+
+        #endregion
+    }
+}
---
+++ Bachup ProductManagment/ProductManagmentWeb/Areas/Admin/Controllers/ExpenseController.cs
@@ -0,0 +1,156 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using ProductManagment_DataAccess.Repository.IRepository;
+using ProductManagment_Models.Models;
+using ProductManagment_Models.ViewModels;
+using System.Data;
+
+namespace ProductManagmentWeb.Areas.Admin.Controllers
+{
+
+    [Area("Admin")]
+    [Authorize]
+    public class ExpenseController : Controller
+    {
+
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        public ExpenseController(IUnitOfWork unitOfWork, IWebHostEnvironment webHostEnvironment)
+        {
+            _unitOfWork = unitOfWork;
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+
+        public IActionResult Index()
+        {
+            List<Expense> objExpenseList = _unitOfWork.Expense.GetAll(includeProperties: "ExpenseCategory").ToList();
+            return View(objExpenseList);
+        }
+
+        [HttpGet]
+        public IActionResult Upsert(int? id)
+        {
+            ExpenseVM ExpenseVM = new()
+            {
+                ExpenseCategoryList = _unitOfWork.ExpenseCategory.GetAll().Select(u => new SelectListItem
+                {
+                    Text = u.ExpenseCategoryName,
+                    Value = u.Id.ToString()
+                }),
+
+                //UserList = _unitOfWork.User.GetAll().Select(u => new SelectListItem
+                //{
+                //    Text = u.FirstName,
+                //    Value = u.Id.ToString()
+                //}),
+                Expense = new Expense()
+            };
+            if (id == null || id == 0)
+            {
+                //create
+                return View(ExpenseVM);
+            }
+            else
+            {
+                //update
+                ExpenseVM.Expense = _unitOfWork.Expense.Get(u => u.Id == id);
+                return View(ExpenseVM);
+            }
+
+        }
+
+        [HttpPost]
+
+        // jayre image levani hoy tyre IFormFile? file Parameter lai levnu
+
+        public IActionResult Upsert(ExpenseVM ExpenseVM, IFormFile? file)
+        {
+            if (ModelState.IsValid)
+            {
+
+                string wwwRootPath = _webHostEnvironment.WebRootPath;
+                if (file != null)
+                {
+                    string fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+                    string productPath = Path.Combine(wwwRootPath, @"images\expense");
+
+                    if (!string.IsNullOrEmpty(ExpenseVM.Expense.ExpenseFile))
+                    {
+                        //delete the old image
+                        var oldImagePath =
+                                    Path.Combine(wwwRootPath, ExpenseVM.Expense.ExpenseFile.TrimStart('\\'));
+
+                        if (System.IO.File.Exists(oldImagePath))
+                        {
+                            System.IO.File.Delete(oldImagePath);
+                        }
+                    }
+
+                    using (var fileStream = new FileStream(Path.Combine(productPath, fileName), FileMode.Create))
+                    {
+                        file.CopyTo(fileStream);
+                    }
+
+                    ExpenseVM.Expense.ExpenseFile = @"\images\expense\" + fileName;
+                }
+
+                if (ExpenseVM.Expense.Id == 0)
+                {
+                    _unitOfWork.Expense.Add(ExpenseVM.Expense);
+                }
+                else
+                {
+                    _unitOfWork.Expense.Add(ExpenseVM.Expense);
+                }
+
+                _unitOfWork.Save();
+                TempData["success"] = "Expense created successfully";
+                return RedirectToAction("Index");
+            }
+            else
+            {
+
+                return View(ExpenseVM);
+            }
+        }
+
+        #region API CALLS
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<Expense> objExpenseList = _unitOfWork.Expense.GetAll(includeProperties: "ExpenseCategory").ToList();
+            return Json(new { data = objExpenseList });
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int? id)
+        {
+            var productToBeDeleted = _unitOfWork.Expense.Get(u => u.Id == id);
+            if (productToBeDeleted == null)
+            {
+                return Json(new { success = false, message = "Error while deleting" });
+            }
+
+            var oldImagePath =
+                           Path.Combine(_webHostEnvironment.WebRootPath,
+                           productToBeDeleted.ExpenseFile.TrimStart('\\'));
+
+            if (System.IO.File.Exists(oldImagePath))
+            {
+                System.IO.File.Delete(oldImagePath);
+            }
+
+            _unitOfWork.Expense.Remove(productToBeDeleted);
+            _unitOfWork.Save();
+
+            return Json(new { success = true, message = "Delete Successful" });
+        }
+
+
+        #endregion
+
+
+    }
+}
