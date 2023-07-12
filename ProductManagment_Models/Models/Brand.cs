74ca34bd06140d56fcfe12e202ecc2ee40892d6e
sunilbotadara <45990446+sunilbotadara@users.noreply.github.com>
2023-07-12 12:09:40 +0530

    Updates

--- ProductManagment_Models/Models/Brand.cs
+++ ProductManagment_Models/Models/Brand.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.EntityFrameworkCore;
-
 namespace ProductManagment_Models.Models;
 
 public partial class Brand
